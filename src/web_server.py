ca11a9902ef10a80576df5e0a2a85fdf008cf9e7
schneemaier <akos.schneemaier@gmail.com>
2026-02-15 16:48:18 -0500

    Rapid commit

--- src/web_server.py
+++ src/web_server.py
@@ -375,8 +375,9 @@ async def app_handler(request):
         })
         # web.Response(text='OK')
         ws_logger.debug(f"Payload {payload1}")
-        #await ws.send_str(payload1)
-        await ws.send_json({'event':'pusher:connection_established','data':'{"socket_id":"265216.826472"}'})
+        minified_json = json.dumps(payload1, separators=(',', ':'))
+        await ws.send_str(minified_json)
+        #await ws.send_json({'event':'pusher:connection_established','data':'{"socket_id":"265216.826472"}'})
         #"{'event': 'pusher:connection_established','data': '{"socket_id":"265216.826472"}'
         return #web.Response(text='OK') #websocket_handler(request)
     else:
