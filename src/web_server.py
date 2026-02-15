422ad953bba0af85205de69c94bf23ae057bbd8f
schneemaier <akos.schneemaier@gmail.com>
2026-02-15 15:10:45 -0500

    Rapid commit

--- src/web_server.py
+++ src/web_server.py
@@ -364,7 +364,7 @@ async def app_handler(request):
     if request.headers.get('Upgrade', '').lower() == 'websocket':
         logger.debug(f"WebSocket upgrade request from : {request.remote}")
         ws = web.WebSocketResponse()
-        await ws.prepare(request)
+        #await ws.prepare(request)
         #clients.add(ws)
         #ws_connected = True
         port = request.transport.get_extra_info('peername')[1]
@@ -373,7 +373,7 @@ async def app_handler(request):
             'event': 'pusher:connection_established',
             'data': '{"socket_id":"265216.826472"}' #I think this defines the socket ID for the connection. Will have to be random number?
         })
-        web.Response(text='OK')
+        await web.Response(text='OK')
         ws_logger.debug(f"Payload {payload1}")
         return await ws.send_str(payload1) #websocket_handler(request)
     else:
