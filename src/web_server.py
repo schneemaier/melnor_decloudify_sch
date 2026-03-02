73c66ee5a9ab0e2093f5c04a7dd7ce73061fee93
schneemaier <akos.schneemaier@gmail.com>
2026-03-01 21:55:28 -0500

    Rapid commit

--- src/web_server.py
+++ src/web_server.py
@@ -79,8 +79,8 @@ def update_states(bin_state, remote_id):
 
     remote_stamp[remote_id] = bin_state[bin_fields['TIME_LOW']] + (bin_state[bin_fields['TIME_HIGH']] * 256)
     time_stamp[remote_id] = remote_stamp[remote_id]
-    unit[1] = bin_state[bin_fields['UNIT_ID_HIGH_1']].hex() + bin_state[bin_fields['UNIT_ID_LOW_1']].hex()
-    unit[2] = bin_state[bin_fields['UNIT_ID_HIGH_2']].hex() + bin_state[bin_fields['UNIT_ID_LOW_2']].hex()
+    unit[1] = hex(bin_state[bin_fields['UNIT_ID_HIGH_1']]) + hex(bin_state[bin_fields['UNIT_ID_LOW_1']])
+    unit[2] = hex(bin_state[bin_fields['UNIT_ID_HIGH_2']]) + hex(bin_state[bin_fields['UNIT_ID_LOW_2']])
     battery1 = bin_state[bin_fields['BATTERY_1']]
     battery2 = bin_state[bin_fields['BATTERY_2']]
     battery[unit1] = battery1 * 1.4428 - 268
