b0d34147062900bd71b7002af8925b6beb3c8b74
schneemaier <akos.schneemaier@gmail.com>
2026-03-03 20:13:26 -0500

    Rapid commit

--- src/web_server.py
+++ src/web_server.py
@@ -101,8 +101,9 @@ def update_states(bin_state, remote_id):
         for i in range(8):
             valve[i] = (button[b] >> i) & 1
             logger.info(f"i {i}, valve[i] {valve[i]} button {hex(button[b])}")
+        logger.info(f"valve before {valve} valves {valves}")
         valves[unit[b]] = valve
-        logger.info(f"valve {valve} valves {valves}")
+        logger.info(f"valve after  {valve} valves {valves}")
     reported_valves[remote_id] = valves
     logger.info(f"BUTTONS: {reported_valves[remote_id]}")
 
